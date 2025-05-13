f6040eb54f856e97f46345793d992fd9d4936b7f
Over-knight <lilvickydsecond@gmail.com>
2025-05-13 17:58:22 +0100

    almost done with recommnation an ai intgration but not on yet

--- src/controllers/recommendationController.ts
+++ src/controllers/recommendationController.ts
@@ -1,25 +1,24 @@
 import { Request, response, Response } from "express";
-import { textModel } from "../lib/genaiClient";
-
-export const getJobRecommendation = async (req: Request, res: Response): Promise<void> {
+// import { textModel } from "../lib/genaiClient";
+import genAI from "../lib/genaiClient";
+export const getJobRecommendation = async (req: Request, res: Response): Promise<void> => {
     try{
-        // const model = genAI.getGenerativeModel({ model: 'gemini-pro'});
+        const model = genAI.getGenerativeModel({ model: 'gemini-pro'});
         const prompt = `
         I am looking for job recommendations based on past applications in backend engineering and Node.js.
         Please return 5 recommended job titles and short descriptions as a JSON array.
         `;
 
-        const result = await textModel.generateContent({
-            model: "gemini-pro",
-            text: prompt
-        });
+        const result = await model.generateContent({prompt});
         const response = await result.response;
         const text = response.text();
 
+        console.log("Raw Gemini response:", text);
+
         const recommendations = JSON.parse(text);
-        res.json({ recommendations });
+        res.status(200).json({ recommendations });
     } catch (error) {
-        console.error(error);
+        console.error("Gemini error",error);
         res.status(500).json({ message: "Failed to get recommendations"});
     }
 };
--- src/lib/genaiClient.ts
+++ src/lib/genaiClient.ts
@@ -1,10 +1,16 @@
-import { GoogleGenAI } from "@google/genai";
+// import { GoogleGenAI } from "@google/genai";
 
-const apiKey = process.env.API_KEY;
-if (!apiKey) {
-    throw new Error("Missing Google API KEY in environment");
-} 
+// const apiKey = process.env.API_KEY;
+// if (!apiKey) {
+//     throw new Error("Missing Google API KEY in environment");
+// } 
 
-const genAI = new GoogleGenAI({ apiKey });
-export const textModel = genAI.models;
+// const genAI = new GoogleGenAI({ apiKey });
+// export const textModel = genAI.models;
 
+// src/utils/geminiClient.ts
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+
+export default genAI;
