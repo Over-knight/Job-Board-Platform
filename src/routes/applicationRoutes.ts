2894db4c794ac6b16f977638f7978584381765db
Over-knight <lilvickydsecond@gmail.com>
2025-05-13 12:52:50 +0100

    added roles to applications

--- src/routes/applicationRoutes.ts
+++ src/routes/applicationRoutes.ts
@@ -4,10 +4,13 @@ import { applyToJob,
     getJobApplications,
     getMyApplications
  } from "../controllers/applicationController";
+import { isEmployer,
+    isCandidate
+ } from "../middleware/roles";
 
  const router = Router();
 
- router.post("/jobs/:jobId/apply", protect, applyToJob);
- router.get("/jobs/:jobId", protect, getJobApplications);
- router.get("/me", protect, getMyApplications);
+ router.post("/jobs/:jobId", protect, isCandidate, applyToJob);
+ router.get("/jobs/:jobId", protect, isEmployer, getJobApplications);
+ router.get("/me", protect, isCandidate, getMyApplications);
  export default router
--- src/routes/jobRoutes.ts
+++ src/routes/jobRoutes.ts
@@ -5,13 +5,16 @@ import { createJob,
     getJobById, 
     deleteJob, 
     updateJob } from "../controllers/jobController";
+import { isCandidate,
+    isEmployer
+ } from "../middleware/roles";
 
 const router = Router();
 
 router.get("/", getJobs);
 router.get("/:id", getJobById);
-router.post("/", protect, createJob);
-router.patch("/:id", protect, updateJob);
-router.delete("/:id", protect, deleteJob);
+router.post("/", protect, isEmployer, createJob);
+router.patch("/:id", protect, isEmployer, updateJob);
+router.delete("/:id", protect, isEmployer, deleteJob);
 
 export default router;
